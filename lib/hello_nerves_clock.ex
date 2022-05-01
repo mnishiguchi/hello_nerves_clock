9d2e18fbef1b5e6ef6d958ea55947227a6aa230c
Masatoshi N <nishiguchi.masa@gmail.com>
2022-05-01 15:47:49 -0400

    Create a Nerves project

---
+++ lib/hello_nerves_clock.ex
@@ -0,0 +1,18 @@
+defmodule HelloNervesClock do
+  @moduledoc """
+  Documentation for HelloNervesClock.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloNervesClock.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
